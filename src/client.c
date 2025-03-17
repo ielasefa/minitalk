53dae68e2103065a7f3de105f518896940ae4e9d
Ilyas El Asefar <ilyass.42.1337@gmail.com>
2025-03-17 03:28:24 +0000

    minitalk

--- src/client.c
+++ src/client.c
@@ -5,23 +5,24 @@ void cheak_input(int n)
 
     if(ac != 3)
     {
-        ft_putstr("Error:[ ./client <pid> <message> ]\n");
+        ft_putstr("Error:[./client <pid> <message> ]\n");
         exit(1);
     }
     if(av[i] < 0)
     {
-        ft_putstr("Error: invalid <PID>",1);
+        ft_putstr("Error:invalid <PID>",1);
         exit(1);
     }
     if(!cheak_digit(av[1]))
     {
-        ft_putstr("Eroor\n av < -1");
+        ft_putstr("Eroor:invalid <PID>");
         exit(1);
     }
 }
 
 int main(int ac ,char **av)
 {
-    int 
+    int pid;
     cheak_input(ac , av);
+    pid = ft_atoi(av[1]);
 }
